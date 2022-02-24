5e2c5a72e4458bac49514951f2da24d9da7e0539
Josiah Victor <victorjosiahm3@gmail.com>
2022-02-24 17:14:49 +0100

    load and connect to mongoDb with mongoose

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const todos = require('./router/todos')
 const homePageRoute = require('./router/homePage')
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 
+mongoose
+  .connect('mongodb://localhost/todo-list-app')
+  .then(() => {
+    console.log('connected to mongoDb...')
+    app.listen(3000, () => {
+      console.log('Listening to port 3000...')
+    })
+  })
+  .catch((err) => console.log('Error->', err))
 const app = express()
 
 app.use(express.json())
 app.use('/', homePageRoute)
 app.use('/api/todos', todos)
-
-app.listen(3000, () => {
-  console.log('Listening to port 3000...')
-})
